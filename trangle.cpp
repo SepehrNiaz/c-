052489a34282c6799efa738d3342f4882b5f5c30
Sepehr Niaz <149871293+SepehrNiaz@users.noreply.github.com>
2023-12-29 13:46:23 +0330

    Add files via upload

---
+++ trangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a,b,c;
+
+    cin>>a>>b>>c;
+
+    if(a*a + b*b == c*c)
+        cout<<"True";
+
+    else
+        cout<<"False";
+
+    return 0;
+}
